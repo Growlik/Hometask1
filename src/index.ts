6c32dfdde4041a3736b72d5524970c10b8f48064
Growlik <ya.ivanovv99@yandex.ru>
2023-06-03 12:42:59 +0200

    Changed port to 3000

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export const app = express()
 const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.use('/videos', videosRouter)
 app.use('/testing', testingRouter)
