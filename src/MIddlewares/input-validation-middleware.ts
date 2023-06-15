429faba0a338881849df52e77f6363b04093aa04
Growlik <ya.ivanovv99@yandex.ru>
2023-06-15 20:26:09 +0200

    minor changes

--- src/MIddlewares/input-validation-middleware.ts
+++ src/MIddlewares/input-validation-middleware.ts
@@ -47,7 +47,7 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
             }
         })
         // res.status(400).json({errors: errors.array()})
-        res.status(400).json({errors: sanitizedErrors})
+        res.status(400).json({errorsMessages: sanitizedErrors})
     } else {
         next()
     }
