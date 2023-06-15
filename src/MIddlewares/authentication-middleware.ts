549f8a93e8d86c8e89b4020a6a71a06f2bcab0d1
Growlik <ya.ivanovv99@yandex.ru>
2023-06-15 17:32:44 +0200

    minor changes

--- src/MIddlewares/authentication-middleware.ts
+++ src/MIddlewares/authentication-middleware.ts
@@ -1,11 +1,17 @@
 import {NextFunction, Request, Response} from "express";
+import {usersRepository} from "../Repositories/users-repository";
 
 
+// export const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//     const {username, password} = req.body
+//     if (!(username === 'admin' && password === 'qwerty')) {
+//         res.sendStatus(401)
+//     } else {
+//         next();
+//     }
+// }
 export const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const {username, password} = req.body
-    if (!(username === 'admin' && password === 'qwerty')) {
-        res.sendStatus(401)
-    } else {
-        next();
-    }
+    const authHeader = req.headers['authorization']
+    console.log(authHeader)
+    next;
 }
