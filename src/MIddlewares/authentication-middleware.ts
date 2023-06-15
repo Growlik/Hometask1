3924c521b66f3435e7bcf946e30cf7ba93cc50b8
Growlik <ya.ivanovv99@yandex.ru>
2023-06-15 17:43:46 +0200

    minor changes

--- src/MIddlewares/authentication-middleware.ts
+++ src/MIddlewares/authentication-middleware.ts
@@ -12,7 +12,7 @@ import {usersRepository} from "../Repositories/users-repository";
 export const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization']
     if (authHeader === usersRepository[0].loginPass) {
-    next;
+    next();
     } else {
         res.sendStatus(401)
     }
