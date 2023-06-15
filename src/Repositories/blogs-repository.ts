5a95bc6b2c6986f5d2b7ba22f5cc2df37ad69bcd
Growlik <ya.ivanovv99@yandex.ru>
2023-06-15 19:44:00 +0200

    minor changes

--- src/Repositories/blogs-repository.ts
+++ src/Repositories/blogs-repository.ts
@@ -19,7 +19,7 @@ export const blogsRepository = {
     },
     createBlog(name: string, description: string, websiteUrl: string) {
         const newBlog = {
-            id: +(new Date()).toString(),
+            id: +(new Date().toString()),
             name: name,
             description: description,
             websiteUrl: websiteUrl
